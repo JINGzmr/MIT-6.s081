f743ceb716d95876cc089204ed4c3e51de67889c
JINGzmr <2743377630@qq.com>
2023-11-19 22:09:39 +0800

    update labs

--- user/primes.c
+++ user/primes.c
@@ -3,36 +3,31 @@
 #include "user/user.h"
 #define MAX_NUM 35
 
-void Run_Child(int divisor,
-               int* left_pipefd)  // 子进程递归执行代码 ，参数为该除数
+void Run_Child(int divisor, int* left_pipefd)  // 子进程递归执行代码 ，参数为该除数
 {
     int num = 0, i = 0;
     int nums[MAX_NUM] = {0};
-    int right_pipefd
-        [2];  // 该管道为当前进程从父进程管道读取到的数据并处理后，写给当前进程的子进程的管道
+    int right_pipefd[2];  // 该管道为当前进程从父进程管道读取到的数据并处理后，写给当前进程的子进程的管道
     pipe(right_pipefd);
     close(left_pipefd[1]);
 
-    while (read(left_pipefd[0], &num,
-                sizeof(num))) {  // 只要返回值不为0，则说明写入端还有东西没写完
+    while (read(left_pipefd[0], &num, sizeof(num))) {  // 只要返回值不为0，则说明写入端还有东西没写完
         if (num == divisor) {  // 该数和除数相同，说明是素数
             printf("prime %d\n", num);
-        } else if (num % divisor !=
-                   0) {  // 如果被整除了，则直接淘汰，不进入下一个管道
+        } else if (num % divisor != 0) {  // 如果被整除了，则直接淘汰，不进入下一个管道
             nums[i++] = num;
         }
     }
     close(left_pipefd[0]);
 
-    if (num !=
-        divisor) {  // 如果num最后的值还是和除数相同，说明此时已经将所有的数处理完毕
+    if (num != divisor) {  // 如果num最后的值还是和除数相同，说明此时已经将所有的数处理完毕
         int pid = fork();
 
         if (pid < 0) {
             printf("Fork failed");
             exit(1);
         } else if (pid == 0) {  // 子进程
-            Run_Child(divisor + 1, right_pipefd);
+            Run_Child(nums[0], right_pipefd);//每次将放入管道中最小的数作为除数，否者如果每次都是在上轮除数的基础上+1的话，会出现内存问题
         } else {  // 父进程
             i = 0;
             close(right_pipefd[0]);
